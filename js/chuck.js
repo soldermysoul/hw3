c3e04d58aba708f713885774169a69a43d3c0479
EC2 Default User <ec2-user@ip-172-31-20-68.ec2.internal>
2020-12-21 04:44:30 +0000

    import from Corsair

---
+++ js/chuck.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+
+let url = `https://itcdland.csumb.edu/~milara/ajax/promo/products.php`;
+let response = await fetch(url);
+let data = await response.text();
+var arr = jQuery.parseJSON(data);
+
+
+
+			$("button").on("click",getjoke);
+
+ChuckCategories();
+
+
+function ChuckCategories(){
+		let Category = $("#Display").val();
+	
+		let chuckcats = ["Animal","Career","Celebrity","Dev","Fashion","Food","History","Money","Movie","Music","Political","Religion","Science","Sport","Travel"];
+
+		
+		for (let i = 0; i < chuckcats.length; i++) {
+	 		$("#Display").append(`<option value="${chuckcats[i]}">${chuckcats[i]} </option>`);
+			
+		}
+	
+	
+	
+ }
+ 
+ 
+async function getjoke(){
+
+ 
+let Category = $("#Display").val();
+let url = `https://api.chucknorris.io/jokes/random?category=` + Category.toLowerCase();
+let response = await fetch(url);
+let data = await response.text();
+var arr = jQuery.parseJSON(data);
+console.log(arr['value']);
+
+			
+//return Category;
+$("#What").html(arr['value']);
+			
+}
+
+
+});
